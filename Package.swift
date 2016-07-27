d1d63bb2fcc2121480919c52eadd15c9de7e4e50
James Campbell <jscampbell.05@googlemail.com>
2016-07-27 13:27:27 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "NcursesKit",
     dependencies: [
-      .Package(url:"https://github.com/iachievedit/CNCURSES", majorVersion: 2)
+      .Package(url:"https://github.com/iachievedit/CNCURSES", majorVersion: 0)
     ]
 )
